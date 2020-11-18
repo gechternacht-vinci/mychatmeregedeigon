7824d1fb8f5d03d6a4034ae1793a90c9bd6aced6
gechternacht-vinci <gabriel@agenciavinci.com.br>
2020-11-18 18:46:11 -0300

    chatbox

--- public/main.js
+++ public/main.js
@@ -1,10 +1,23 @@
+function messageBox(message){
+
+    const box= document.createElement("div");
+    box.innerHTML = message;
+    document.getElementById("chat-box").appendChild(box);
+}
+
+
 const socket = io.connect("https://mychatmeregedeigon.herokuapp.com/")
 socket.on("welcome",data => document.querySelector('header').innerHTML = data)
-socket.on("bodyClick",data => console.log(`received: ${data}`));
+socket.on("bodyClick",data => {
+    console.log(`received: ${data}`)
+    messageBox(`received: ${data}`)
+});
 
 document.querySelector('body').addEventListener("click",()=>{
     let messageContent = document.querySelector('input').value;
     socket.emit("bodyClick",messageContent);
+
+    messageBox(`sending message:${messageContent}`)
     console.log(`sending message:${messageContent}`);
 })
 
